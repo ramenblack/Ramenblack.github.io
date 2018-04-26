b58374f7e5fcc9c09e7a9a75f02c2664f70b1f21
Keegan Westphal <31550979+ramenblack@users.noreply.github.com>
2018-04-26 12:17:13 -0700

    Add files via upload

---
+++ keegy/index.php
@@ -0,0 +1,332 @@
+<!DOCTYPE html>
+<html lang="en">
+<title>Keegy.ioe</title>
+<link rel="shortcut icon" href="html/IMG/favicon.png" type="image/x-icon">
+  <meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="/css/keegy.css">
+<link rel="stylesheet" href="/css/keegy1.css">
+<link rel="stylesheet" href="/css/font.css">
+ <link rel="stylesheet" href="/css/bootstrap.css">
+<link rel="stylesheet" href="/css/googlefont.css">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<script src="css/jquery.js"></script>
+<meta name="viewport" content="width=device-width, initial-scale=10">
+
+ <style>
+  body {
+      font: 20px Montserrat, sans-serif;
+      line-height: 1.8;
+      color: #f5f6f7;
+  }
+body, html {
+    height: 100%;
+    color: #777;
+    line-height: 1.8;
+}
+.bgimg-1 {
+    background-attachment: fixed;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+}
+.bgimg-1 {
+    background-image: url('/html/IMG/seattle.jpg');
+    min-height: 100%;
+}
+     .w3-wide {letter-spacing: 10px;}
+.w3-hover-opacity {cursor: pointer;}
+}
+.title { 
+	background-color: #7E7E7E;
+	color: #7E7E7E;
+} 
+}
+input type="text" {
+    width: 100%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    box-sizing: border-box;
+    border: 4px solid #7F7F7F;
+    border-radius: 4px;
+}
+textarea {
+	resize: none;
+    width: 100%;
+    height: 200px;
+    padding: 12px 20px;
+    margin: 8px 0;
+    box-sizing: border-box;
+    border: 1px solid #7F7F7F;
+    border-radius: 4px;
+}
+
+h1{
+	text-align:center;
+}
+comment{
+
+	font-size: 20px;
+	font-family:'Palanquin', sans-serif;
+	
+}
+
+
+
+  </style>
+<body>
+<div class="w3-top">
+<nav class="navbar navbar-default" style="font-family: 'Palanquin', sans-serif;">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+         <a class="navbar-brand" href="/index.html">keegy.ioe</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav navbar-right">
+		  <li><a href="/login/index.php">login</a></li>
+          <li><a href="/index.php#About Me">about me</a></li>
+          <li><a href="/index.php#Portfolio">portfolio</a></li>   
+           <li><a href="/html">html</a></li> 
+           <li><a href="/za">phpadmin</a></li> 
+          <li><a href="/index.php#Let's get in touch">let's get in touch</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+
+</div>
+
+</div>
+ <body>
+<h4 id="Keegy"></h4>
+<div class="bgimg-1 w3-display-container w3-opacity-min" id="home">
+  <div class="w3-display-middle" style="white-space:nowrap; font-family: 'Lobster', cursive;">
+    <span class="w3-center w3-padding-large w3-black w3-xlarge w3-wide w3-animate-opacity">Hi. <span class="w3-hide-small">I'm</span> Keegan.</span>
+  </div>
+</div>
+<h4 id="About Me"></h4>
+<br />
+<br>
+<div style="font-family: 'Palanquin', sans-serif;">
+	<P><Center><font color="#7E7E7E" size="20">About me.</font></Center></P>
+<CENTER><img src="/html/IMG/keegg.jpg" align="center" width="200" height="200"></CENTER>
+	<br>
+    <h2><center><font color="#7E7E7E">Who I am.</font></center></h2>  
+    <p><center><font color="#7E7E7E"> My name is Keegan. I am a guitarist, freelance web developer, and artist. I am a proud father of two (dogs). Like everything I do, I consider my projects to be ultimately meaningless, a mear shout into the void that is our existance. I'm also a big meme boy! Is doge still relevant?</font></center></p>
+  <hr>
+</div>
+
+<article class="w3-container" style="font-family: 'Palanquin', sans-serif;"> 
+		<div class="data-scroll-reveal="enter bottom over 1s">
+    <h2><center><font color="#7E7E7E">What I do.</font></center></h2>
+  <p><center><font color="#7E7E7E"> Html is my jam! I'm also learning python and java. I enjoy building websites, and user interfaces. This website was my first major endeavour, and has been my main project for quite some time now.</font></center></p>
+  <hr>
+</article>
+</div>
+<div class="post">
+<section class="w3-container" style="font-family: 'Palanquin', sans-serif;"> 
+    <h2><center><font color="#7E7E7E">What I strive for.</font></center></h2>
+  <p><center><font color="#7E7E7E">   I intend for this website to be a jumping off point for my html projects. I intend to start building websites as a part time job in the near future. This website is simply a place where I can learn html and hone my skills.</font></center></p>
+  <hr>
+  </div>
+  <div class="post" style="font-family: 'Palanquin', sans-serif;">
+     <p><center><font color="#7E7E7E">learn more: <a href="/html/projects/Guitar.html">I play guitar</a>, <a href="/html/projects/Coffee.html">I drink coffee</a>, <a href="/html/projects/dogs.html">I like dogs</a>.
+     </div>
+</section>
+  <h4 id="Portfolio"/h4>
+ <div class="w3-metro-darken" style="font-family: 'Palanquin', sans-serif;">
+    <div class="w3-container w3-content w3-padding-64 post">
+<P><Center><font color="#FFFFFF" size="20">Portfolio.</font></Center></P>
+      <p class="w3-opacity w3-center"><i>all photos taken and processed by me</i></p><br>
+<div class="w3-row" id="myGrid" style="margin-bottom:128px">
+  <div class="w3-third">
+    <img src="\html\IMG\guitaro.png" style="width:100%">
+    <img src="\html\IMG\gate.JPG" style="width:100%">
+       <img src="\html\IMG\sign3.PNG" style="width:100%">
+  </div>
+
+  <div class="w3-third">
+   <img src="\html\IMG\doghand.JPG" style="width:100%">
+    <img src="\html\IMG\guitarhand.PNG" style="width:100%">
+    <img src="\html\IMG\flowers.png" style="width:100%">
+  </div>
+
+  <div class="w3-third">
+     <img src="\html\IMG\theatre.PNG" style="width:100%">        
+      <img src="\html\IMG\bloom1.PNG" style="width:100%">
+      <img src="\html\IMG\windos.jpg" style="width:100%">
+  </div>
+</div><h4 id="Projects"/h4>
+</div>  
+    </div>
+  
+ <div class="w3-metro-red" style="font-family: 'Palanquin', sans-serif;">
+    <div class="w3-container w3-content w3-padding-64 post">
+<P><Center><font color="#FFFFFF" size="20">Projects.</font></Center></P>
+<br>
+<p class="w3-opacity w3-center"><i>complete index of my html and python endeavours.</i></p><br>
+<div class="w3-row" id="myGrid" style="margin-bottom:128px">
+<br>
+<br>
+<br>
+ <div class="w3-row w3-center" id="myGrid" style="margin-bottom:128px margin-width:5px" >
+    <div class="w3-third" style="margin-right:0px">
+       <a href="html/"><img src="\html\IMG\html.png"/ title="html"/ class="w3-round w3-margin-bottom" height="80" width="80" margin-left:0px margin-right:0px/></a>
+    </div>
+    <div class="w3-third" style="margin-width:0px">
+       <a href="/python"><img src="\html\IMG\python.png"/ title="python"/ class="w3-round w3-margin-bottom" height="80" width="80" margin-left:0px margin-right:0px/></a>
+    </div>
+    <div class="w3-third" style="margin-left:0px">
+        <a href="/logs"><img src="\html\IMG\wood.png"/ title="logs"/ class="w3-round w3-margin-bottom" height="80" width="80" margin-left:0px margin-right:0px/></a>
+    </div>
+</div>
+</div>
+</div>
+</div>
+<div class="w3-container" style="font-family: 'Palanquin', sans-serif;">
+    <h4 id="Let's get in touch"/h4>
+		<br>
+		<br>
+		<br>
+		<div style="font-family: 'Palanquin', sans-serif;">
+    	<P><Center><font color="#7E7E7E" size="20">Let's get in touch.</font></Center></P>
+	<br>
+	<br>
+	<?php  
+if(empty($_POST['name'])){  $name_input = "Name";  } 
+	else {	$name_input = 	$_POST['name'];	}
+	
+if(empty($_POST['comment'])){  $comment_input = "Comment";  } 
+	else {	$comment_input = 	$_POST['comment'];	}
+?>
+    <h2><font color="#7E7E7E">Leave a Comment.</font></h2>
+    <br>
+<comment>
+<form  method="post">
+<br>
+
+&nbsp;	   Name
+	<br>
+<input type="text" name="name" maxlength="20"> 
+<br>
+<br>
+
+  Comment <textarea name="comment" maxlength="500" rows="2" cols="10" style="line-height:200%;"></textarea>
+</comment>
+<button class="w3-button w3-blue-gray w3-round-xlarge">Submit</button><button class="w3-button w3-blue-grey w3-round-xlarge" style="float:right; height:46px;width:100px;"><a href = "select.php" target="_blank" style="text-decoration: off; color:white;">Logs</a></button>
+</form>
+<br> 
+<P><font color="#7E7E7E" size="5">Or Reach Me Via My Socials.</font></P>
+    
+    <div class="w3-row w3-left" id="myGrid" style="margin-bottom:128px margin-width:5px" >
+    <div class="w3-third" style="margin-right:0px">
+       <a href= "https://ramenblack.github.io"><img src="\html\IMG\web1.png" title="Github Site"/ class="w3-round w3-margin-bottom" height="50" width="50" margin-right:0px/>
+    </div>
+    <div class="w3-third" style="margin-width:0px">
+        <a href= "https://www.instagram.com/keeganwestphal"><img src="\html\IMG\instagram1.png"/ title="Instagram"/ class="w3-round w3-margin-bottom" height="50" width="50" margin-left:0px margin-right:0px/></a>
+    </div>
+    <div class="w3-third" style="margin-left:0px">
+           <a href= "https://www.Github.com/ramenblack/"><img src="\html\IMG\github1.png"/ title="Github"/ class="w3-round w3-margin-bottom" height="50" width="50" margin-left:0px/></a>
+    </div>
+    <br></div></div></div>
+   <br>
+
+</div>
+</div>
+     </div>
+</div>
+
+<div id="googleMap" style="height:400px;" class="w3-grayscale-max"></div>
+<script>
+function myMap() {
+  myCenter=new google.maps.LatLng(47.608013, -122.335167);
+  var mapOptions= {
+    center:myCenter,
+    zoom:12, scrollwheel: false, draggable: false,
+    mapTypeId:google.maps.MapTypeId.ROADMAP
+  };
+  var map=new google.maps.Map(document.getElementById("googleMap"),mapOptions);
+  var marker = new google.maps.Marker({
+    position: myCenter,
+  });
+  marker.setMap(map);
+}
+</script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDm7dxDV86SXBYufvAcaDDnn2ZNffkbmuE&callback=myMap"></script>
+
+  <hr>
+
+</div>
+</body>
+
+</section>
+  <footer>
+	  <footer class="w3-center">
+	   <div class="w3-white">
+	  <a href="#Keegy" class="w3-button w3-light-grey"><i class="fa fa-arrow-up w3-margin-right"></i>To the top</a>
+      <print><center><font size="2" color="#7E7E7E"> copyright © 2017 keegan westphal. depression and the depression logo are property of keegan westphal | all rights reserved</font></center> </print>
+      <p><font size="2" color="#7E7E7E">Powered by <a href="https://www.w3schools.com/w3css/default.asp" title="W3.CSS" target="_blank" class="w3-hover-text-green">w3.css</a></p>
+</footer>
+</html>
+
+<?php
+
+if ( $name_input != "Name" ){
+//sql input	*******************************	
+
+
+if (!($connection = mysqli_connect($hostname,$username, $password))) die("Could not connect to database");
+ mysqli_select_db( $connection,"stream");
+
+
+
+	#$result = mysqli_query( $conn,$sql) or die('Could not look up user information; ' . mysqli_error($conn));
+	$result = mysqli_query ( $connection, "SELECT CURDATE();");
+	$row = mysqli_fetch_row($result);$date = $row[0];
+   
+	$result = mysqli_query ($connection,"SELECT CURTIME();" );
+	$row = mysqli_fetch_row($result);$time = $row[0];
+
+	$str0 = '';
+	$str1 = $_SERVER['REMOTE_ADDR'];
+	$str2 = $time;
+	$str3 = $date;
+	$str4 = $name_input;  
+	$str5 =  $comment_input;
+//echo " $str1 : $date : $time <br>"; 
+$query = "INSERT INTO stream.contact (`id`, `ip`, `time`, `date`, `name`, `comment`) VALUES ('' ,'$str1','$str2','$str3','$str4','$str5');";
+
+ #$result = mysqli_query ($connection,$query)  or showerror();	
+ $result = mysqli_query ($connection,$query);		
+	mysqli_close($connection);
+} // end if  $name_input != "Name"
+
+
+// ******************************************
+
+//<?php
+//require("constants.php");
+
+// 1. Create a database connection
+//$connection = mysqli_connect(DB_SERVER,DB_USER,DB_PASS);
+//if (!$connection) {
+//    die("Database connection failed: " . mysqli_error());
+
+
+// 2. Select a database to use 
+//$db_select = mysqli_select_db($connection, DB_NAME);
+//if (!$db_select) {
+//    die("Database selection failed: " . mysqli_error());
+//}
+//
+
+
+// *******************************************		
+
+?>
